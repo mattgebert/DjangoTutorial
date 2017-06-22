d0baf1638ddd291df76f3c048fc7210a6c697842
Matt Gebert <mattheuu.gebert@gmail.com>
2017-06-23 01:44:55 +1000

    Added support for Postgres and use of psycopg.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = ['128.199.119.243',
 		 '0.0.0.0',
 		 'photafy.me',
 		 '192.168.1.:',
+		 '192.168.0.17',
                  '220.240.161.97',
                  'localhost',
                  '127.0.0.1',
@@ -85,8 +86,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE'	: 'django.db.backends.postgresql_psycopg2',
+	'NAME'		: 'djangotutorial',
+	'USER'		: 'matt',
+	'PASSWORD' 	: 'password',
+	'HOST'		: 'localhost',
+	'PORT'		: '',
+       # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -127,5 +133,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = BASE_DIR + "/static_root"
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,"static_root/")
+STATIC_URL = "/static/"
+#STATIC_URL = '/static/'
